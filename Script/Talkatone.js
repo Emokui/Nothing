3f40290a26553e3c463e5fb39e9abbdb16101f44
𝓶𝓮𝓰𝓾𝓶𝓲 <136175327+Emokui@users.noreply.github.com>
2025-04-24 12:01:19 +0800

    Update Talkatone.js

--- Script/Talkatone.js
+++ Script/Talkatone.js
@@ -1,13 +1,15 @@
 let body = $response.body;
 
-body = body.replace(/<div id="top-ad-banner".*?<\/div>/g, '');
+const adPatterns = [
+  /<div id="(?:top|middle|bottom)-ad-banner".*?<\/div>/gs,
+  /<div class="ad-container".*?<\/div>/gs
+];
 
-body = body.replace(/<div id="middle-ad-banner".*?<\/div>/g, '');
+adPatterns.forEach(pattern => {
+  body = body.replace(pattern, '');
+});
 
-body = body.replace(/<div id="bottom-ad-banner".*?<\/div>/g, '');
-
-body = body.replace(/padding-top:.*?;/g, 'padding-top: 0px;'); // 删除顶部空白
-body = body.replace(/padding-bottom:.*?;/g, 'padding-bottom: 0px;'); // 删除底部空白
-body = body.replace(/<div class="ad-container".*?<\/div>/g, ''); // 针对通用广告容器
+body = body.replace(/padding-top:.*?;/g, 'padding-top: 0px;');
+body = body.replace(/padding-bottom:.*?;/g, 'padding-bottom: 0px;');
 
 $done({ body });
