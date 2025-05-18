60520592ad076a8e96a2d78c8206b527b886c87a
𝑭𝒂𝒕𝒂𝒍𝒆𝒗𝒆𝒍 <136175327+Emokui@users.noreply.github.com>
2025-05-18 15:21:33 +0800

    Update wireguard.sh

--- Shell/wireguard.sh
+++ Shell/wireguard.sh
@@ -281,9 +281,9 @@ main_menu() {
         print_separator
         echo -e "${YELLOW}${BOLD}1.${RESET} ${GREEN}生成免费账户配置${RESET}"
         echo -e "${YELLOW}${BOLD}2.${RESET} ${BLUE}获取团队账户配置${RESET}"
-        echo -e "${YELLOW}${BOLD}3.${RESET} ${BOLD}查看当前配置${RESET}"
+        echo -e "${YELLOW}${BOLD}3.${RESET} ${BLUE}查看当前配置${RESET}"
         echo -e "${YELLOW}${BOLD}4.${RESET} ${RED}删除所有配置及依赖${RESET}"
-        echo -e "${YELLOW}${BOLD}0.${RESET} 退出"
+        echo -e "${YELLOW}${BOLD}0.${RESET} ${GREEN}退出${RESET}"
         print_separator
         printf "${BOLD}请输入选项 [0-4]: ${RESET}"
         read -r choice
