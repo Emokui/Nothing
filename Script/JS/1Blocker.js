4cd4a6d96f7a9d9777dc140b5c700f93fc1e7be4
𝓯𝓪𝓽𝓪𝓵 <136175327+Emokui@users.noreply.github.com>
2025-06-13 14:13:47 +0800

    更新 1Blocker.js

--- Script/JS/1Blocker.js
+++ Script/JS/1Blocker.js
@@ -1,5 +1,16 @@
-// 1Blocker Premium Unlock Script (Deobfuscated)
-// Target: RevenueCat API v1/v2 endpoints
+/***********************************
+
+> ScriptName 1Blocker
+> Author @neko
+
+[rewrite_local]
+^https:\/\/api\.(revenuecat|rc-backup)\.com\/.+\/(receipts$|subscribers\/[^/]+$) url script-response-body https://raw.githubusercontent.com/Emokui/Nothing/Zero/Script/1Blocker.js
+^https:\/\/api\.(revenuecat|rc-backup)\.com\/.+\/(receipts|subscribers) url script-request-header https://raw.githubusercontent.com/Emokui/Nothing/Zero/Script/deleteheader.js
+
+[mitm]
+hostname=api.revenuecat.com, api.rc-backup.com
+
+***********************************/
 
 const appMapping = {
   '1Blocker': { 
