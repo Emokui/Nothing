d7649fdfa955086f43de5f0e9871187d648cf16d
Mahito <136175327+Emokui@users.noreply.github.com>
2026-01-17 20:59:12 +0800

    Update mihomo.sh

--- Shell/mihomo.sh
+++ Shell/mihomo.sh
@@ -149,14 +149,12 @@ log-level: silent
 ipv6: true
 dns:
   enable: true
-  listen: 127.0.0.1:1053
+  listen: ::1:1053
   ipv6: true
   nameserver:
     - 1.1.1.1
     - 2606:4700:4700::1111
   enhanced-mode: fake-ip
-  fake-ip-range: 198.18.0.1/16
-  fake-ip-range6: fdfe:dcba:9876::1/64
 profile:
   store-selected: false
   store-fake-ip: false
